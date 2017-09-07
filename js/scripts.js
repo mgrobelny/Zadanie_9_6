15a1100b89b8a8188920104875ed05e38d6da3c8
mgrobelny <michalmaciejg@gmail.com>
2017-09-07 18:31:22 +0200

    initial

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+//przypisanie przycisku do zmiennej "add":
+var add = document.getElementById('addElem'),
+
+//zapamiętanie w zmiennej "list" odniesienia do elementu z id 'list':
+    list = document.getElementById('list');
+
+//nadanie listenera przyciskowi i ustawienie wykonania funkcji po kliknięciu:
+add.addEventListener('click', function(e) {
+
+//przypisanie metody createElement do zmiennej element:
+    var element = document.createElement('li');
+
+//przypisanie dla utworzonych elementów "li" tekstu 'Item' oraz kolejnej liczby: 
+    element.innerText = 'Item ' + ((document.getElementsByTagName('li').length -1) + 1);
+
+//dodawanie elementu do zmiennej "list" na końcu:
+    list.appendChild(element);
+})
